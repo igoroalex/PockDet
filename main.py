ad6c5f4141bc78d484e3059a646c02b6a364a926
GoRo <igoroalex@gmail.com>
2021-10-13 17:02:16 +0300

    FIX class Card, ADD hand police

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class Deck:
     def __init__(self):
         with open("dangerous_ties.json", "r") as file:
             data = json.load(file)
-        self.__all_cards = {card["name"]: card for card in data["cards"]}
+        self.__all_cards = {card["id_card"]: card for card in data["cards"]}
 
     def __str__(self):
         return f"{self.__all_cards}"
@@ -28,19 +28,19 @@ DECK = Deck()
 
 
 class Card:
-    def __init__(self, id_card: str):
-        self.id_card = id_card
-        data_card = DECK.all_cards.get(id_card, {})
-        self.name = data_card.get("name", "")
-        self.pile = data_card.get("pile", "")
-        self.time = data_card.get("time", 0)
-        self.picture = data_card.get("picture", "")
-        self.daughters = data_card.get("daughters", [])
-        self.limits = data_card.get("limits", [])
-        self.rate = data_card.get("rate", 0)
-
-    def __str__(self):
-        return f"Card {self.id_card} with {self.daughters=}"
+    @classmethod
+    def get_card(cls, id_card: str):
+        data_card = {
+            "id_card": "i1",
+            "time": 0,
+            "daughters": [],
+            "next_card": "",
+            "up_time": 0,
+            "police": 0,
+            "rate": 0,
+        }
+        data_card.update(DECK.all_cards.get(id_card, {}))
+        return data_card
 
 
 class Hand:
@@ -50,23 +50,48 @@ class Hand:
         self.opened_cards = set()
         self.available_cards = {first_card}
         self.time_left = 0
+        self.police = 0
+        self.police_cards = ["p6", "p5", "p4", "p3", "p2", "p1"]
 
-    def play_card(self, id_card):
-        card = Card(id_card)
-        if card.id_card in self.available_cards:
-            self.time_left += 0 if card.id_card in self.opened_cards else 1
-            self.opened_cards.add(card.id_card)
-            self.available_cards.update([_ for _ in card.daughters])
-            print(f"Played {card}. Time left {self.time_left}")
-            print(rf"dangerous_ties\{card.picture}")
-            webbrowser.open(rf"dangerous_ties/{card.picture}")
-        else:
-            print(f"{card} not exist, available cards: {self.available_cards}")
+    def next_cards(self):
+        return self.available_cards - self.opened_cards
+
+    def want_card(self, id_card):
+        card = Card.get_card(id_card)
+
+        if card["id_card"] in self.opened_cards:
+            self.show_card(card)
+            return
+
+        if card["id_card"] not in self.available_cards:
+            print(f"{card} not available, your next cards: {self.next_cards()}")
+            return
+
+        self.play_card(card)
+
+        if card["police"]:
+            card["next_card"] = self.police_cards.pop()
+            self.available_cards.add(card["next_card"])
+
+        if card["next_card"]:
+            self.want_card(card["next_card"])
+
+    def show_card(self, card):
+        print(f"Played {card}. Time left {self.time_left}, police {self.police}")
+        webbrowser.open(rf"dangerous_ties/{card['id_card']}.jpg")
+
+    def play_card(self, card):
+        self.time_left += card["time"]
+        self.police += card["police"]
+        self.opened_cards.add(card["id_card"])
+        self.available_cards.update([_ for _ in card["daughters"]])
+
+        self.show_card(card)
 
 
 if __name__ == "__main__":
 
-    print(DECK)
+    # print(DECK)
 
     user_name = "goro"
     current_user = User(user_name)
@@ -75,9 +100,9 @@ if __name__ == "__main__":
     hand = Hand(wanted_card)
 
     while True:
-        hand.play_card(wanted_card)
+        hand.want_card(wanted_card)
 
-        print(hand.opened_cards)
+        # print(hand.opened_cards)
         print(hand.available_cards)
 
         wanted_card = input("wanted card:").lower()
