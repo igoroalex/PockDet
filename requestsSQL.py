bd3791984a4ca8ec7c326a54c0cd50617ec09404
GoRo <igoroalex@gmail.com>
2021-10-29 11:58:25 +0300

    REFACTOR add def db_fetchall, db_commit

--- requestsSQL.py
+++ requestsSQL.py
@@ -5,30 +5,42 @@ from psycopg.rows import dict_row
 
 from config import CONNINFO
 
-# from hand import Hand
 
-
-def get_data_hand(user_name: str) -> dict_row:
+def db_fetchall(request: str, args_request: dict) -> dict_row:
     conn = psycopg.connect(conninfo=CONNINFO)
 
     try:
         with conn:
             with conn.cursor(row_factory=dict_row) as cur:
-                cur.execute("SELECT * FROM hands WHERE user_name = %s;", (user_name,))
+                cur.execute(request, args_request)
                 data_sql = cur.fetchall()
     finally:
         conn.close()
     return data_sql
 
 
-def save_db(hand):
+def db_commit(request: str, args_request: dict):
     conn = psycopg.connect(conninfo=CONNINFO)
 
     try:
         with conn:
             with conn.cursor(row_factory=dict_row) as cur:
-                cur.execute(
-                    """INSERT INTO hands VALUES (%(user_name)s, 
+                cur.execute(request, args_request)
+                conn.commit()
+    finally:
+        conn.close()
+
+
+def get_data_hand(user_name: str) -> dict_row:
+
+    return db_fetchall(
+        "SELECT * FROM hands WHERE user_name = %(user_name)s;", {"user_name": user_name}
+    )
+
+
+def save_db(hand):
+    db_commit(
+        """INSERT INTO hands VALUES (%(user_name)s, 
                                                     %(opened_cards)s, 
                                                     %(available_cards)s, 
                                                     %(time_left)s, 
@@ -37,31 +49,23 @@ def save_db(hand):
                                                     %(last_card)s, 
                                                     %(jail)s, 
                                                     %(rate)s);""",
-                    {
-                        "user_name": hand.user_name,
-                        "opened_cards": json.dumps(list(hand.opened_cards)),
-                        "available_cards": json.dumps(list(hand.available_cards)),
-                        "time_left": hand.time_left,
-                        "police": hand.police,
-                        "police_cards": json.dumps(list(hand.police_cards)),
-                        "last_card": hand.last_card,
-                        "jail": hand.jail,
-                        "rate": hand.rate,
-                    },
-                )
-                conn.commit()
-    finally:
-        conn.close()
+        {
+            "user_name": hand.user_name,
+            "opened_cards": json.dumps(list(hand.opened_cards)),
+            "available_cards": json.dumps(list(hand.available_cards)),
+            "time_left": hand.time_left,
+            "police": hand.police,
+            "police_cards": json.dumps(list(hand.police_cards)),
+            "last_card": hand.last_card,
+            "jail": hand.jail,
+            "rate": hand.rate,
+        },
+    )
 
 
 def update_db(hand):
-    conn = psycopg.connect(conninfo=CONNINFO)
-
-    try:
-        with conn:
-            with conn.cursor(row_factory=dict_row) as cur:
-                cur.execute(
-                    """UPDATE hands SET user_name = %(user_name)s, 
+    db_commit(
+        """UPDATE hands SET user_name = %(user_name)s, 
                                 opened_cards = %(opened_cards)s, 
                                 available_cards = %(available_cards)s, 
                                 time_left = %(time_left)s, 
@@ -71,21 +75,18 @@ def update_db(hand):
                                 jail = %(jail)s, 
                                 rate = %(rate)s
                             WHERE user_name = %(user_name)s;""",
-                    {
-                        "user_name": hand.user_name,
-                        "opened_cards": json.dumps(list(hand.opened_cards)),
-                        "available_cards": json.dumps(list(hand.available_cards)),
-                        "time_left": hand.time_left,
-                        "police": hand.police,
-                        "police_cards": json.dumps(list(hand.police_cards)),
-                        "last_card": hand.last_card,
-                        "jail": hand.jail,
-                        "rate": hand.rate,
-                    },
-                )
-                conn.commit()
-    finally:
-        conn.close()
+        {
+            "user_name": hand.user_name,
+            "opened_cards": json.dumps(list(hand.opened_cards)),
+            "available_cards": json.dumps(list(hand.available_cards)),
+            "time_left": hand.time_left,
+            "police": hand.police,
+            "police_cards": json.dumps(list(hand.police_cards)),
+            "last_card": hand.last_card,
+            "jail": hand.jail,
+            "rate": hand.rate,
+        },
+    )
 
 
 def save_hand(hand):
